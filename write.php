c1f8308435fdfee111d6402adc993b5cd2dc3ee2
Sangseung <lss9209@naver.com>
2020-07-21 09:06:47 +0900

    complete3

--- write.php
+++ write.php
@@ -19,17 +19,21 @@
      ?>
   </nav>
   <article class = "col-md-9">
-    <form action="/process.php" method="post">
-      <p class="writeTool">글 제목:
-        <input type="text" name="title"/>
-      </p>
-      <p class="writeTool">작성자명:
-        <input type="text" name="author"/>
-      </p>
-      <div class="wrtieTool" id="bContent">글 내용:</div>
-      <div><textarea name="content" cols="40" rows="10"></textarea></div>
-      <input type="submit" value="작성완료"></button>
-    </form>
+    <div id="writep">
+      <form action="/process.php" method="post">
+        <p class="writeTool">글 제목:
+          <input type="text" name="title"/>
+        </p>
+        <p class="writeTool">작성자명:
+          <input type="text" name="author"/>
+        </p>
+        <div class="wrtieTool" id="bContent">글 내용:</div>
+        <div><textarea name="content" cols="40" rows="10"></textarea></div>
+        <input type="submit" value="작성완료"></button>
+      </form>
+  </div>
+    <button type="button" class="btn btn-primary btn-lg" onclick="document.getElementById('body').className='white'">white</button>
+    <button type="button" class="btn btn-primary btn-lg" onclick="document.getElementById('body').className='black'">black</button
   </article>
 </div>
 </body>
